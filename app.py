349d471665e3edc459fc9a93a24853093eb0cefc
Tonho <antonioreverso.com@gmail.com>
2025-02-28 03:57:23 -0300

    add image transformation function to convert files to PNG format

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
+import os, sys
 from PIL import Image
 im = Image.open("test.jpg")
+
+print(im.format, im.size, im.mode)
+
+def transform_into_png():
+  for infile in sys.argv[1:]:
+    f, e = os.path.splitext(infile)
+    outfile = f + ".png"
+    if infile != outfile:
+      try:
+        with Image.open(infile) as im:
+          im.save(outfile)
+      except OSError:
+        print("Cannot Convert: ",infile)
+transform_into_png()
+
