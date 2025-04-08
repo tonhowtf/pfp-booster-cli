c951ead411812c247f8b3b5820cc160f62db4e1b
Tonho <antonioreverso.com@gmail.com>
2025-04-07 21:54:16 -0300

    refactor: enhance image processing by adding size definitions and improving PNG conversion

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os, sys
-from PIL import Image
+from PIL import Image, ImageOps
 from PIL import ImageFilter
+#test
+sizes = {'discord':'128x128',
+         'bluesky': '400x400',
+         'telegram': '512x512',
+         'twitter': '400x400',
+         'whatsapp': '192x192'}
+
 
 im = Image.open("test.jpg")
 low = Image.open("low-quality.jpg")
-print(im.format, im.size, im.mode)
-print(low.format, low.size, low.mode)
-low.crop()
+# print(im.format, im.size, im.mode)
+# print(low.format, low.size, low.mode)
+print(os.path.basename())
 
 def transform_into_png():
   for infile in sys.argv[1:]:
@@ -16,12 +23,22 @@ def transform_into_png():
       try:
         with Image.open(infile) as im:
           im.save(outfile)
+          return outfile
       except OSError:
-        print("Cannot Convert: ", infile)
-        
-
-def img_enhancement():
-  pass
+        print("Não foi possivel converter: ", infile)
+  return None
 
 def pfp_resizer():
-  pass
+  outfile = transform_into_png()
+  if outfile is None:
+    print('Nenhuma imagem encontrada.')
+    return 0
+  with Image.open(outfile) as im:
+    base_name = os.path.splitext(os.path.basename(outfile))
+    for platform, size_str in sizes.items():
+      try:
+        width, height = map(int, size_str.split('x'))
+      except ValueError:
+        print(f"{platform}: {size_str}")
+
+
